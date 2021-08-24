4c6950bfffcee1e744753750c1844e5f3d067977
Exusiai <2246834404@qq.com>
2021-08-24 22:00:31 +0800

    new python bfs with binary heap

--- src/links.rs
+++ src/links.rs
@@ -1,11 +1,11 @@
-// ? Divide and conquer:
+// ! Divide and conquer:
 // ? https://leetcode.com/problems/count-of-range-sum/discuss/1378661/rust-divide-and-conquer-using-prefix-sum?
 
-// ? Binary index tree:
+// ! Binary index tree:
 // ? https://leetcode.com/problems/range-sum-query-mutable/discuss/1385209/rust-clean-binary-index-tree
 // ? https://leetcode.com/problems/reverse-pairs/discuss/1400758/Rust-Binary-Index-Tree?
 
-// ? Dynamic Programming:
+// ! Dynamic Programming:
 // ? https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/discuss/1363872/Rust-general-DP-of-stock-problems
 // ? https://leetcode.com/problems/race-car/discuss/1388604/rust-dp-python-bfs
 // ? https://leetcode.com/problems/domino-and-tromino-tiling/discuss/1390413/rust-dp-with-detailed-recursive-relation
@@ -13,18 +13,28 @@
 // ? https://leetcode.com/problems/super-ugly-number/discuss/1349583/Rust-dp?
 // ? https://leetcode.com/problems/longest-valid-parentheses/discuss/1415072/Rust-DP
 
-// ? Math:
+// ! Math:
 // ? https://leetcode.com/problems/smallest-good-base/discuss/1392797/rust-logn-using-math?
 
-// ? Bit Mask/Manipulation:
+// ! Bit Mask/Manipulation:
 // ? https://leetcode.com/problems/number-of-1-bits/discuss/1337476/rust-bit-maskmanipulation?
 
-// ? Binary Search:
+// ! Binary Search:
 // ? https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/discuss/1361953/rust-binary-search
 // ? https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1356509/rust-binary-search-0ms?
 
-// ? Quick Select:
+// ! Quick Select:
 // ? https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/1370405/rust-median-of-medians-worst-case-on?
 
-// ? Binary Heap/Priority Queue:
+// ! Binary Heap/Priority Queue:
 // ? https://leetcode.com/problems/find-k-pairs-with-smallest-sums/discuss/1359522/Rust-Binary-Heap?
+// ? https://leetcode.com/problems/swim-in-rising-water/discuss/1420956/Python-Typical-DFS-using-Binary-Heap
+// ? https://leetcode.com/problems/trapping-rain-water-ii/discuss/1423470/Python-BFS-using-Binary-Heap
+
+// ! BFS:
+// ? https://leetcode.com/problems/word-ladder/discuss/1418892/Rust-BFS
+// ? https://leetcode.com/problems/word-ladder/discuss/1418892/Rust-BFS-and-Python-Bidirectional-Search
+// ? https://leetcode.com/problems/trapping-rain-water-ii/discuss/1423470/Python-BFS-using-Binary-Heap
+
+// ! DFS:
+// ? https://leetcode.com/problems/swim-in-rising-water/discuss/1420956/Python-Typical-DFS
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,64 @@
 fn main() {
     // playground
-    println!("");
+
+    println!(
+        "{}",
+        Solution::ladder_length(
+            "aa".to_string(),
+            "bb".to_string(),
+            vec!["ab".to_string(), "bb".to_string(),],
+        )
+    );
 }
 
 struct Solution {}
 
-impl Solution {}
+use std::collections::{HashMap, VecDeque};
+impl Solution {
+    pub fn ladder_length(begin_word: String, end_word: String, mut word_list: Vec<String>) -> i32 {
+        let (n, wordlen) = (word_list.len(), begin_word.len()); // wordlen must be uniform across all words
+        let maskmap: HashMap<String, Vec<usize>> = Self::construct_dict(&word_list, wordlen);
+        word_list.push(begin_word); // initialization
+        let mut deque: VecDeque<(usize, i32)> = VecDeque::from(vec![(n, 1)]);
+        let mut seen: Vec<bool> = vec![false; n + 1]; // indicating whether word_list[i] is visited
+
+        while let Some((cur, step)) = deque.pop_front() {
+            let word = &word_list[cur];
+            if word == &end_word {
+                return step;
+            }
+
+            // for every word, push all the possible nexts that
+            // differs by 1 letter using the pre-computed hashmap
+            for i in 0..wordlen {
+                let mask = format!("{}_{}", &word[..i], &word[i + 1..]);
+                if let Some(nexts) = maskmap.get(&mask) {
+                    for &next in nexts {
+                        if seen[next] == false {
+                            seen[next] = true;
+                            deque.push_back((next, step + 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        0
+    }
+
+    // construct a hashmap with masks as keys, and values which are possible routes
+    // to the end_word, while storing words using index i as word_list[i]
+    fn construct_dict(word_list: &Vec<String>, length: usize) -> HashMap<String, Vec<usize>> {
+        let mut maskmap: HashMap<String, Vec<usize>> = HashMap::new();
+
+        for i in 0..word_list.len() {
+            for j in 0..length {
+                let word = &word_list[i];
+                let mask = format!("{}_{}", &word[..j], &word[j + 1..]);
+                maskmap.entry(mask).or_insert(vec![]).push(i);
+            }
+        }
+
+        maskmap
+    }
+}
